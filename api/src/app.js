582ddf0f34fd987f6fed41e3580bbc0425b9b41d
sahilparmar-growexxer <sahil.parmar@growexx.com>
2026-04-15 13:12:16 +0530

    credentials: true error solve

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,8 @@ const allowedOrigins = Array.isArray(env.corsOrigin)
       .filter(Boolean);
 
 const corsOptions = {
-  origin: ["http://localhost:3000" , 'http://127.0.0.1:3000']
+  origin: ["http://localhost:3000" , 'http://127.0.0.1:3000'],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
